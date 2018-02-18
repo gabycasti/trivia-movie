16dda22c9304ae820b887873479f6877eb606971
gabycasti <castillg@gmail.com>
2018-02-18 17:04:32 -0300

    subiendo conexion local storage

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,7 +1,3 @@
-const $form = $('#search-form');
-const $searchField = $('#search-keyword');
-const $responseContainer = $('#response-container');
-
 
 $(function() {
 
@@ -14,90 +10,42 @@ $(function() {
   });
 });
 
+/*$(function () {
+
+  $.ajax({
+    type: 'GET',
+    url:'https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=boolean',
+    success: function(data) {
+     // addNews(data)
+      console.log('success',data)
+
+    },
+   // error: function() {
+      //alert('error con la api');
+    }
+  });
+});*/
 
 
-/*const form = document.getElementById('search-form');
-const searchField = document.getElementById('search-keyword');
-const datos_pokemon = document.getElementById('datos_pokemon');
-let searchedForText;
 
-//a nuestro form le damos el evento submit
-form.addEventListener('submit', function (e){
-  e.preventDefault();
-  datos_pokemon.innerHTML = '';
- // searchedForText = searchField.value;
-  getPokemon();
-})
+/*function addNews(news){
+  console.log(news);
+  const articles = news.response.docs;
+ articles.forEach(function(article){
+ const title = article.headline.main;
+ const snippet = article.snippet;
+  let $li = $('<li />').addClass('articleClass').text(title +'\n'+snippet);
 
-//en la funcion getPokemon haremos nuestras peticiones
-function getPokemon() {
-  const articleRequest = new XMLHttpRequest();
-  //articleRequest.open('GET', `https://pokeapi.co/api/v2/pokemon/${searchedForText}`);
-  articleRequest.open('GET', `https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=boolean&encode=base64`);
-  articleRequest.onload = addNewPokemon;
-  articleRequest.onerror = handleError;
-  articleRequest.send();
-  console.log(articleRequest);
+  $responseContainer.append($li);
+
+  });
+  
 }
 
-//funcion de error
-/*function handleError() {
-  console.log('Se ha presentado un error');
-}
 
-//llamando la data
-function addNewPokemon() {
-  const data = JSON.parse(this.responseText);
-  console.log(data);
-
-  //console.log(article);
-
-//Buscando y agregando nombre del pokemon
-let  nom =  document.createElement('span');
- nom.className = 'titulo'
-const nombre = [];
-  for (let i=0; i < data.forms.length; i++ ){
-    nombre.push(data.forms[i].name);
-  }
-  nom.innerText = nombre;
-
-//Buscando y agregando habilidad de pokemon
-let li = document.createElement('span');
- li.className = 'texto'
-  const pokemon = [];
-  for (let i=0; i < data.abilities.length; i++ ){
-    pokemon.push(data.abilities[i].ability.name);
-  }
-  li.innerText = pokemon;
-
-// Experiencia del pokemon
- let exp =  document.createElement('span');
- exp.innerText = data.base_experience;
- exp.className = "texto"
-//obteniendo la imagen del pokemon
-  let img = document.createElement('img');
-  img.className = 'img2-responsive'
-  let picture = data.sprites.front_female;
-  img.src = picture;
-
-  let hab = document.createElement('span');
-   hab.innerText = 'Habilidad';
-   hab.className = 'titulo2';
-
- let expTxt = document.createElement('span');
-   expTxt.innerText = 'Experiencia';
-   expTxt.className = 'titulo2'
-
-  datos_pokemon.appendChild(hab);
-  datos_pokemon.appendChild(nom);
-  datos_pokemon.appendChild(img);
-  datos_pokemon.appendChild(hab);
-  datos_pokemon.appendChild(li);
-  datos_pokemon.appendChild(expTxt);
-  datos_pokemon.appendChild(exp);
-}
-
-*/
+function handleError(){
+    console.log('Se ha presentado un error');
+}*/
 
 
 
---
+++ assets/app/local_storage.js
@@ -0,0 +1,61 @@
+$( document ).ready(function(){
+    
+    $("#sesion").click(function() {
+     //let usuario = $("#usuario").val();
+     let datos = obtener_localstorage();
+     let name = $("#usuario").val();
+     let pasword = $("#clave").val();
+     if (name == datos.usuario && pasword == datos.clave){
+        document.location.replace('trivia.html')
+     }else if(name == ""){
+       $("#mensaje_usuario").fadeIn();
+       return false;
+     }else{
+        $("#mensaje_usuario").fadeOut();
+         if(pasword == ""){
+        $("#mensaje_clave").fadeIn();
+       return false;
+     }else{
+        $("#mensaje_clave").fadeOut();
+         if(pasword.length < 6 ){
+        $("#mensaje_clave2").fadeIn();
+       return false;
+    }else{
+        $("#mensaje_clave2").fadeOut();
+         if(pasword == "123456"){
+        $("#mensaje_clave3").fadeIn();
+       return false;
+        }
+       }
+      }
+     }
+  })
+
+});
+
+
+guardar_localstorage();
+
+
+function obtener_localstorage(){
+
+    if(localStorage.getItem("nombre")){
+    //let nombre = localStorage.getItem("nombre");
+    let persona = JSON.parse(localStorage.getItem("persona"));
+    return persona;
+    }else
+    console.log("no hay entradas")
+};
+
+
+
+
+function guardar_localstorage(){
+    let persona = {
+        usuario: "gabycasti",
+        clave: "45675693"
+    }
+    //localStorage.setItem("nombre",nombre);
+    localStorage.setItem("persona",JSON.stringify(persona));
+}
+
